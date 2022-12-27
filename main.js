d4d9b936bd9d44d01b1307915630800604ffcf37
mykknight <you@example.com>
2022-12-27 22:08:54 +0530

    expenseWaxios

--- main.js
+++ main.js
@@ -1,21 +1,64 @@
 var btn = document.getElementById('submit');
-
+var p=false;
+var w;
 btn.addEventListener('click',addvalue);
+
+
+axios.get('https://crudcrud.com/api/f221db3395264605b24bf7553e1847e4/expense')
+.then((response) => {
+    console.log(response);
+    for(var i=0; i<response.data.length; i++){
+        prtdata(response.data[i]);
+    }
+})
+.catch((err) => {
+    console.log(err);
+});
+
 
 function addvalue(e){
     e.preventDefault();
-    
     var expense = document.getElementById('number').value;
     var des = document.getElementById('text').value;
     var opt = document.getElementById('option').value;
-
-    //console.log(expense+des+opt);
     
+    var myobj = {
+        expense: expense,
+        description: des,
+        option: opt
+    };
+    if(myobj.expense=='' || myobj.description=='') alert("Please enter the details");
+    else{
+        prtdata(myobj);
+        if(p) {
+        axios.put(`https://crudcrud.com/api/f221db3395264605b24bf7553e1847e4/expense/${w}`,myobj)
+        .then((response) => {
+            console.log(response);
+            p=false;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        }
+        else {
+        axios.post('https://crudcrud.com/api/f221db3395264605b24bf7553e1847e4/expense', myobj)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+    }
+}
+
+function prtdata(myobj){
+
     var ui = document.getElementById('details');
 
     var li = document.createElement('li');
 
-    li.appendChild(document.createTextNode(expense+" - "+des+" - "+opt+" "));
+    li.appendChild(document.createTextNode(myobj.expense+" - "+myobj.description+" - "+myobj.option+" "));
 
     var dlt = document.createElement('input');
     dlt.type = 'button';
@@ -24,10 +67,6 @@ function addvalue(e){
     li.appendChild(dlt);
 
     dlt.addEventListener('click',dltexp);
-    function dltexp(e){
-        e.preventDefault();
-        li.remove();
-    }
 
     var edt = document.createElement('input');
     edt.type='button';
@@ -38,14 +77,24 @@ function addvalue(e){
 
     edt.addEventListener('click',editdts);
 
+    function dltexp(e){
+        console.log(myobj._id);
+        axios.delete(`https://crudcrud.com/api/f221db3395264605b24bf7553e1847e4/expense/${myobj._id}`);
+        li.remove();
+    }
+    
     function editdts(e){
         e.preventDefault();
-
-        expense.value=expense;
-        des.value=des;
-        opt.value=opt;
+    
+        document.getElementById('number').value = myobj.expense;
+        document.getElementById('text').value = myobj.description;
+        document.getElementById('option').value = myobj.option;
+        p=true;
+        w=myobj._id;
+        
         li.remove();
     }
 
     ui.append(li);
 }
+
